20bed65386d1cbdcf2bfddc59cf350b90deec300
vegicle <vegicle@gmail.com>
2020-07-06 07:16:54 -0700

    Cart Fixes

--- mac/mac/settings.py
+++ mac/mac/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 
+    'user.apps.UserConfig',
     'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from user import views as UserViews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
+    path('', include('shop.urls')),
+    path("user/", include('user.urls')),
+
+    path('login/', UserViews.login_form, name='login_form'),
+    path('signup/', UserViews.signup_form, name='login_form'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
